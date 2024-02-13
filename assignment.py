d3c7dd728f8fde978f0020085b07b58000a1851c
byrocuy <byrocuy@gmail.com>
2024-02-13 19:26:31 +0700

    Added argparser for terminal input

--- assignment.py
+++ assignment.py
@@ -1,6 +1,13 @@
+import argparse
+import os
 import pandas as pd
 import numpy as np
 from datetime import datetime
+
+def validate_file(file_path):
+    if not os.path.exists(file_path):
+        raise ValueError(f'File not found: {file_path}')
+    return file_path
 
 def count_downtime(df):
     i, total_down = 0, 0
@@ -11,13 +18,23 @@ def count_downtime(df):
 
     return total_down
 
-df = pd.read_csv('device_network_data.csv')
+def main(params):
+    file_path = params.file_path        
 
-df.loc[:, "Value"] = df.Value.replace("Null", np.nan)
-df.loc[:, "Value"] = df.Value.ffill()
+    df = pd.read_csv(file_path)
 
-df["Value"] = df.Value.astype(int)
-df["date_time"] = df.loc[:, "Epoch Time"].apply(lambda x: datetime.fromtimestamp(x / 1e3))
+    df.loc[:, "Value"] = df.Value.replace("Null", np.nan)
+    df.loc[:, "Value"] = df.Value.ffill()
 
-downtime = count_downtime(df)
-print(f'Total Downtime: {downtime} seconds')
+    df["Value"] = df.Value.astype(int)
+    df["date_time"] = df.loc[:, "Epoch Time"].apply(lambda x: datetime.fromtimestamp(x / 1e3))
+
+    downtime = count_downtime(df)
+    print(f'Total Downtime: {downtime} seconds')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Calculate total downtime')
+
+    parser.add_argument('--file_path', default='device_network_data.csv', help='Path to the file', required=True, type=validate_file)
+    args = parser.parse_args()
+    main(args)
